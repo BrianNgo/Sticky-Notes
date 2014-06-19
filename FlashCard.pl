8fd952f7f1e39f6039ce25c67d36eda2b6c3acd9
BrianNgo <buu1989@yahoo.com>
2014-06-19 15:41:56 -0700

    almost done

--- FlashCard.pl
+++ FlashCard.pl
@@ -2,27 +2,47 @@
 use strict;
 use warnings;
 use Tk;
+use DateTime;
 
-my $filename = "inputFile.txt";
+my $fileName = "inputFile.txt";
+my $mw = MainWindow->new; # Create new main window
+$mw->geometry("500x300+0+0"); # Position on the left corner
+$mw->title(getDate());
+my $textField = $mw->Text(-background => "yellow",
+			  -font => "verdana 15")->pack; 
+checkFile($fileName);
 
-if (-e $filename) { # Check whether the file exist
-    my $mw = MainWindow->new; # Create new main window
-    $mw->geometry("500x300+0+0"); # Position on the left corner
-    print "File Exists!\n";
-    open(fileHandler, $filename);
-    while (my $input = <fileHandler>){
-	chomp($input);
-	print "$input\n";
-	$mw->Label(-text => $input)->pack;
-    }
-    close(fileHandler);
-    $mw->Button(-text => "Quit", -command => sub{ exit})->pack;
-    MainLoop;
-    #open(fileHandler, '>>', $filename);
-    #my $consoleInput = <STDIN>;
-    #print fileHandler $consoleInput;
-    #close(fileHandler);
+my $input = "";
+$mw->bind("<Return>" => sub {$input = $textField->get("1.0", "end");}); 
+MainLoop;
+
+# Save the new strings to input file
+if (!($input eq "")) {
+    open(fileHandler,">", $fileName);
+    print fileHandler $input;
 }
-else {
-    print "File not exist!\n";
+
+# getDate function returns the current date 
+sub getDate {
+    my $dt = DateTime->now;
+    my $date = $dt->ymd;
+    return $date;
+}
+
+# checkFile function checks for the existence of input file
+sub checkFile {
+    my $file = $_[0];
+    # Check for the saved input file
+    if (-e $file) {    
+	print "File Exists!\n";
+	open(fileHandler, $file);
+	while (my $input = <fileHandler>){
+	    chomp($input);
+	    $textField->insert("end", "$input\n");
+	}
+	close(fileHandler);	
+    }
+    else {
+	print "File not exist!\n";
+    }
 }
